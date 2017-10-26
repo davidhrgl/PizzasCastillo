0bed3a2c3ab240d15083c70dbdbb0830dfeddf93
davidhrgl <davidhrgl@gmail.com>
2017-10-26 10:20:28 -0500

    Esta es la primera entrada del projecto subiendo todos los archivos al repositorio

---
+++ src/main/java/com/escom/pizzascastillo/FXMLController.java
@@ -0,0 +1,25 @@
+package com.escom.pizzascastillo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Aplicacion Funcionando en MAVEN y Java Fx!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
