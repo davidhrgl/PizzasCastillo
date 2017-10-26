ed970760a8a4cd705c55ce7a776de16e28769d5b
davidhrgl <davidhrgl@gmail.com>
2017-10-26 13:24:36 -0500

    Prueba2: Commit, QUITANDO un "Hola Mundo" en la clase FXMLController.java

--- src/main/java/com/escom/pizzascastillo/FXMLController.java
+++ src/main/java/com/escom/pizzascastillo/FXMLController.java
@@ -16,7 +16,6 @@ public class FXMLController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Aplicacion Funcionando en MAVEN y Java Fx!");
-        label.setText("Hola Mundo");
     }
     
     @Override
